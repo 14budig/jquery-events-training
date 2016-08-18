fb1f98d712a9dd2b9fac6c8b8d90ca60c86aa883
14budig <nbudig@gmail.com>
2016-08-18 15:38:46 -0700

    solves "change"

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -4,4 +4,21 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
+  $('.add').on('change', function(){
+    var leftNum = $('#left').val();
+    var rightNum = $('#right').val();
+    if(leftNum === ""){
+      leftNum = 0;
+    }
+    else{
+      leftNum = parseInt(leftNum);
+    }
+    if(rightNum === ""){
+      rightNum = 0;
+    }
+    else{
+      rightNum = parseInt(rightNum);
+    }
+    $('#total').val(leftNum+rightNum);
+  });
 })
