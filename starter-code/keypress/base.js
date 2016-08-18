3525809321aeaa6d42b15f9328264c65eb9e8fbe
14budig <nbudig@gmail.com>
2016-08-18 16:06:27 -0700

    solves "keypress"

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,5 +1,22 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
+  var counting = false;
+  var startTime;
+  var totalTime;
+  $('body').keyup(function(e){
+   if(e.keyCode == 32){
+     if(counting){
+       counting = false;
+       totalTime = Date.now() - startTime;
+       console.log(totalTime);
+       var seconds = totalTime/1000;
+       $('span').html(seconds + " seconds");
+     }
+     else{
+       counting = true;
+       startTime = Date.now();
+     }
+   }
+});
 })
