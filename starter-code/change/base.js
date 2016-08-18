8f3250c194366a760bdb01fa1b759d143e0903fe
14budig <nbudig@gmail.com>
2016-08-18 16:21:27 -0700

    solves the bonus for "change"

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -3,6 +3,11 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 
   $("#time").text( Date.now() );
+  $('<button name="reset" id="reset">Reset</button').appendTo('body');  //Text doesn't work?  Look into this for later
+  $('#reset').text("Reset");                //Quick and dirty fix
+  $('#reset').bind("click", function(){
+    $('input').val("");
+  });
 
   $('.add').on('change', function(){
     var leftNum = $('#left').val();
