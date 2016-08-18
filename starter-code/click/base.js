257810573f6c3253209d068f3f015ee4de1aee99
14budig <nbudig@gmail.com>
2016-08-18 15:50:55 -0700

    solves "click" with the bonus

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,8 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
-})
+  $('span').on('click', function(){
+    var text = $(this).html();
+    $('<li>' + text + ' at ' + Date.now() + '</li>').appendTo('ul');
+  });
+});
