7ef0642676d0434cbbebd0108bd0ed7615603f64
14budig <nbudig@gmail.com>
2016-08-18 15:14:05 -0700

    solves "submit" with bonus

--- starter-code/submit/base.js
+++ starter-code/submit/base.js
@@ -3,7 +3,10 @@ console.log("Sanity Check: JS is working!");
 var rick_roll_youtube_embed_code = "<iframe width='420' height='315' src='https://www.youtube.com/embed/dQw4w9WgXcQ' frameborder='0' allowfullscreen></iframe>";
 
 $(document).ready(function(){
-
+  $('form').submit(function(event){
+    event.preventDefault();
+    $(rick_roll_youtube_embed_code).appendTo('body');
+  });
   $("#time").text( Date.now() );
 
 })
